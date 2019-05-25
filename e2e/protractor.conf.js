7fef8e266907595f0a8368cf2576fbd8c9e37cb6
henmo <henmor1987@gmail.com>
2019-05-25 21:01:02 +0300

    ss

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://3.16.89.86:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
